20b24d40cbdfb0d9738f8e06b2aa87a6a3228a91
rysk1013 <rysk2908@gmail.com>
2022-10-19 10:51:31 +0900

    add menu validation

---
+++ src/app/Http/Requests/Mneu/MenuRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Mneu;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MenuRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'menu_name' => ['required'],
+            'subcategory' => ['required'],
+            'turn' => ['required'],
+            'price' => ['required','numeric'],
+        ];
+    }
+}
